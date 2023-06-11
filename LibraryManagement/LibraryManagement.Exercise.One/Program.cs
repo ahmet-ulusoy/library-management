2d5080d66f2cd83406c9788bc43b3ed0f29b5870
Ahmet Ulusoy <ahmetulusoy@hotmail.com>
2023-06-11 23:05:26 +0300

    Initial commit

---
+++ LibraryManagement/LibraryManagement.Exercise.One/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LibraryManagement.Exercise.One
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
