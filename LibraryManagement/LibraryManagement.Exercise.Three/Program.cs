0bfc603dfdecdbd881aefb24564c9282407e79b5
Ahmet Ulusoy <ahmetulusoy@hotmail.com>
2023-06-12 00:43:35 +0300

    Third project created, project structure and folders created.

---
+++ LibraryManagement/LibraryManagement.Exercise.Three/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace LibraryManagement.Exercise.Three
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to library manager, exercise three");
+
+            Console.WriteLine();
+        }
+    }
+}
