d60312ce96d43f0172b7761fb6ca84e0aca559e8
Ahmet Ulusoy <ahmetulusoy@hotmail.com>
2023-06-11 23:16:24 +0300

    BaseLibraryItem created

---
+++ LibraryManagement/LibraryManagement.Exercise.One/Models/BaseLibraryItem.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibraryManagement.Exercise.One.Models
+{
+    public abstract class BaseLibraryItem : EntityBase<long>
+    {
+        [Required]
+        public string Name { get; set; } = null!;
+
+        [Required, MinLength(13), MaxLength(13)]
+        public string Isbn { get; set; } = null!;
+
+        [Required]
+        public bool IsDownloadable { get; set; }
+
+        public string? DownloadLink { get; set; }
+    }
+}
