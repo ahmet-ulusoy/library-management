0fecd865670ea22c0ef2db72f9aefff59cb30c9c
Ahmet Ulusoy <ahmetulusoy@hotmail.com>
2023-06-12 00:05:22 +0300

    ServiceProvider and Registration of BookService created.

--- LibraryManagement/LibraryManagement.Exercise.Two/Program.cs
+++ LibraryManagement/LibraryManagement.Exercise.Two/Program.cs
@@ -1,4 +1,7 @@
 ﻿using LibraryManagement.Exercise.Two.Models;
+using LibraryManagement.Exercise.Two.Services.Abstract;
+using LibraryManagement.Exercise.Two.Services.Concrete;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace LibraryManagement.Exercise.Two
 {
@@ -6,11 +9,39 @@ namespace LibraryManagement.Exercise.Two
     {
         public static ICollection<Book> Books { get; set; } = new List<Book>();
 
+        private static ServiceProvider? _serviceProvider;
+
+        private static IBookService? _bookService;
+
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to library manager, exercise two");
 
             Console.WriteLine();
+
+            RegisterServices();
+
+            GetBookService();
+        }
+
+        private static void RegisterServices()
+        {
+            _serviceProvider = new ServiceCollection()
+                .AddSingleton<IBookService, BookService>()
+                .BuildServiceProvider();
+        }
+
+        private static void GetBookService()
+        {
+            if (_serviceProvider != null)
+            {
+                _bookService = _serviceProvider.GetRequiredService<IBookService>();
+
+                if (_bookService == null)
+                {
+                    throw new InvalidOperationException($"{nameof(_bookService)} is null.");
+                }
+            }
         }
     }
 }
