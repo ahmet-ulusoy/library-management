0bf4e243e3dbd36a695cd20fb48607f6668b92ed
Ahmet Ulusoy <ahmetulusoy@hotmail.com>
2023-06-12 00:52:27 +0300

    BookLocation created.

---
+++ LibraryManagement/LibraryManagement.Exercise.Three/Models/BookLocation.cs
@@ -0,0 +1,23 @@
+﻿namespace LibraryManagement.Exercise.Three.Models
+{
+    public class BookLocation
+    {
+        public long Id { get; set; }
+
+        public long BookId { get; set; }
+
+        public long RoomId { get; set; }
+
+        public long RowId { get; set; }
+
+        public long ShelfId { get; set; }
+
+        public virtual Book Book { get; set; } = null!;
+
+        public virtual Room Room { get; set; } = null!;
+
+        public virtual Row Row { get; set; } = null!;
+
+        public virtual Shelf Shelf { get; set; } = null!;
+    }
+}
--- LibraryManagement/LibraryManagement.Exercise.Three/Program.cs
+++ LibraryManagement/LibraryManagement.Exercise.Three/Program.cs
@@ -4,10 +4,11 @@ namespace LibraryManagement.Exercise.Three
 {
     public static class Program
     {
-        public static List<Room> Rooms { get; set; } = new List<Room>();
-        public static List<Row> Rows { get; set; } = new List<Row>();
-        public static List<Shelf> Shelves { get; set; } = new List<Shelf>();
-        public static List<Book> Books { get; set; } = new List<Book>();
+        public static ICollection<Room> Rooms { get; set; } = new List<Room>();
+        public static ICollection<Row> Rows { get; set; } = new List<Row>();
+        public static ICollection<Shelf> Shelves { get; set; } = new List<Shelf>();
+        public static ICollection<Book> Books { get; set; } = new List<Book>();
+        public static ICollection<BookLocation> BookLocations { get; set; } = new List<BookLocation>();
 
         public static void Main(string[] args)
         {
