2cabf03ed851b321bb1688ad7224dc594e0494c2
Ahmet Ulusoy <ahmetulusoy@hotmail.com>
2023-06-11 23:58:10 +0300

    Book created.

---
+++ LibraryManagement/LibraryManagement.Exercise.Two/Models/Book.cs
@@ -0,0 +1,15 @@
+﻿namespace LibraryManagement.Exercise.Two.Models
+{
+    public class Book
+    {
+        public List<string> Authors { get; set; } = new List<string>();
+
+        public string Title { get; set; } = null!;
+
+        public string Publisher { get; set; } = null!;
+
+        public int PublicationYear { get; set; }
+
+        public int NumberOfPages { get; set; }
+    }
+}
