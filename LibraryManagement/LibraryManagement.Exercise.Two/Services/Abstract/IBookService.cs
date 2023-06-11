fc1f2de2564139a3826df88832926cdeecf0c580
Ahmet Ulusoy <ahmetulusoy@hotmail.com>
2023-06-12 00:01:41 +0300

    IBookService and BookService created.

---
+++ LibraryManagement/LibraryManagement.Exercise.Two/Services/Abstract/IBookService.cs
@@ -0,0 +1,10 @@
+﻿using LibraryManagement.Exercise.Two.Models;
+
+namespace LibraryManagement.Exercise.Two.Services.Abstract
+{
+    public interface IBookService
+    {
+        List<Book> ReadBooks(string input);
+        List<Book> FindBooks(string searchString);
+    }
+}
---
+++ LibraryManagement/LibraryManagement.Exercise.Two/Services/Concrete/BookService.cs
@@ -0,0 +1,18 @@
+﻿using LibraryManagement.Exercise.Two.Models;
+using LibraryManagement.Exercise.Two.Services.Abstract;
+
+namespace LibraryManagement.Exercise.Two.Services.Concrete
+{
+    public class BookService : IBookService
+    {
+        public List<Book> FindBooks(string searchString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Book> ReadBooks(string input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
