8b551492b04271a4ec9ccd1ab1a059b8caeb6cdf
Ahmet Ulusoy <ahmetulusoy@hotmail.com>
2023-06-11 23:36:41 +0300

    Library created.

---
+++ LibraryManagement/LibraryManagement.Exercise.One/Models/Library.cs
@@ -0,0 +1,13 @@
+﻿namespace LibraryManagement.Exercise.One.Models
+{
+    public static class Library
+    {
+        public static ICollection<Author> Authors { get; set; } = new List<Author>();
+
+        public static ICollection<Publisher> Publishers { get; set; } = new List<Publisher>();
+
+        public static ICollection<Book> Books { get; set; } = new List<Book>();
+
+        public static ICollection<BookAuthor> BookAuthors { get; set; } = new List<BookAuthor>();
+    }
+}
