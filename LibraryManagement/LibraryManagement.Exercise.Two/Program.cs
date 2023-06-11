ca2c7c6ba48e921e2a6b13c1f099878c8fe1719e
Ahmet Ulusoy <ahmetulusoy@hotmail.com>
2023-06-11 23:49:52 +0300

    Second project created.

---
+++ LibraryManagement/LibraryManagement.Exercise.Two/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LibraryManagement.Exercise.Two
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to library manager, exercise two");
+        }
+    }
+}
