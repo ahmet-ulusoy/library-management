24129d7e9e85772d74da19716a6cbc84f9c45e3e
Ahmet Ulusoy <ahmetulusoy@hotmail.com>
2023-06-12 00:00:14 +0300

    Program update, input file created.

--- LibraryManagement/LibraryManagement.Exercise.Two/Program.cs
+++ LibraryManagement/LibraryManagement.Exercise.Two/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace LibraryManagement.Exercise.Two
+﻿using LibraryManagement.Exercise.Two.Models;
+
+namespace LibraryManagement.Exercise.Two
 {
     public static class Program
     {
+        public static ICollection<Book> Books { get; set; } = new List<Book>();
+
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to library manager, exercise two");
+
+            Console.WriteLine();
         }
     }
 }
